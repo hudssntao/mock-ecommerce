a4b53c7b6fcd220a45b1513ac171966e39d1852c
hudssntao <hudsontao@gmail.com>
2025-09-21 17:43:10 -0400

    feat(lib): update orm getter functionality

---
+++ src/lib/orm.ts
@@ -0,0 +1,18 @@
+import { MikroORM, EntityManager } from '@mikro-orm/core';
+import config from '../mikro-orm.config';
+
+let ormInstance: MikroORM | undefined;
+
+const getORM = async (): Promise<MikroORM> => {
+  if (!ormInstance) {
+    ormInstance = await MikroORM.init(config);
+  }
+  return ormInstance;
+};
+
+const getEM = async (): Promise<EntityManager> => {
+  const orm = await getORM();
+  return orm.em.fork();
+};
+
+export { getORM, getEM };
